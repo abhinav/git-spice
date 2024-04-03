af4a8e3c26819e6006cc695446f07b7e2e601a4a
Abhinav Gupta <mail@abhinavg.net>
2024-04-02 20:56:08 -0700

    root: move back to main, rename to mainCmd

--- main.go
+++ main.go
@@ -12,6 +12,8 @@ import (
 	"github.com/alecthomas/kong"
 	"github.com/mattn/go-isatty"
 	"github.com/rs/zerolog"
+	"go.abhg.dev/gs/internal/gh"
+	"golang.org/x/oauth2"
 )
 
 var _version = "dev"
@@ -37,7 +39,7 @@ func main() {
 		}
 	}()
 
-	var cmd rootCmd
+	var cmd mainCmd
 	parser, err := kong.New(&cmd,
 		kong.Name("gs"),
 		kong.Description("gs is a command line tool to manage stacks of GitHub pull requests."),
@@ -101,4 +103,61 @@ func main() {
 	parser.FatalIfErrorf(err)
 
 	kctx.FatalIfErrorf(kctx.Run())
+}
+
+type globalOptions struct {
+	Token string `name:"token" env:"GITHUB_TOKEN" help:"GitHub API token; defaults to $GITHUB_TOKEN"`
+}
+
+type mainCmd struct {
+	globalOptions
+
+	// Flags with side effects whose values are never accesssed directly.
+	Verbose bool               `short:"v" help:"Enable verbose output"`
+	Dir     kong.ChangeDirFlag `short:"C" placeholder:"DIR" help:"Change to DIR before doing anything"`
+	Version versionFlag        `help:"Print version information and quit"`
+
+	Repo struct {
+		Init repoInitCmd `cmd:"" aliases:"i" help:"Initialize a repository for stacking"`
+	} `cmd:"" aliases:"r" group:"Repository"`
+
+	Branch struct {
+		Track   branchTrackCmd   `cmd:"" aliases:"tr" help:"Begin tracking a branch with gs"`
+		Untrack branchUntrackCmd `cmd:"" aliases:"utr" help:"Stop tracking a branch with gs"`
+
+		// Creation and destruction
+		Create branchCreateCmd `cmd:"" aliases:"c" help:"Create a new branch"`
+		Delete branchDeleteCmd `cmd:"" aliases:"de" help:"Delete the current branch"`
+
+		// Mutation
+		Edit   branchEditCmd   `cmd:"" aliases:"e" help:"Edit the current branch"`
+		Rename branchRenameCmd `cmd:"" aliases:"r" help:"Rename the current branch"`
+	} `cmd:"" aliases:"b" group:"Branch"`
+
+	Commit struct {
+		Create commitCreateCmd `cmd:"" aliases:"c" help:"Create a new commit"`
+		Amend  commitAmendCmd  `cmd:"" aliases:"a" help:"Amend the current commit"`
+	} `cmd:"" aliases:"c" group:"Commit"`
+
+	Up       upCmd       `cmd:"" group:"Movement" help:"Move up the stack"`
+	Down     downCmd     `cmd:"" group:"Movement" help:"Move down the stack"`
+	Top      topCmd      `cmd:"" group:"Movement" help:"Move to the top of the stack"`
+	Bottom   bottomCmd   `cmd:"" group:"Movement" help:"Move to the bottom of the stack"`
+	Checkout checkoutCmd `cmd:"" aliases:"co" group:"Movement" help:"Checkout a specific pull request"`
+}
+
+func (cmd *mainCmd) AfterApply(kctx *kong.Context, logger *zerolog.Logger) error {
+	if cmd.Verbose {
+		*logger = logger.Level(zerolog.DebugLevel)
+	}
+
+	var tokenSource oauth2.TokenSource = &gh.CLITokenSource{}
+	if token := cmd.Token; token != "" {
+		tokenSource = oauth2.StaticTokenSource(
+			&oauth2.Token{AccessToken: token},
+		)
+	}
+
+	kctx.BindTo(tokenSource, (*oauth2.TokenSource)(nil))
+	return nil
 }
