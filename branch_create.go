be6d64e74f2414ec4c7ebd2100ec8c5143a642b1
Abhinav Gupta <mail@abhinavg.net>
2024-04-06 08:51:50 -0700

    branch create: Add --insert flag
    --insert and --below in combination allow us to add a branch
    above or below the current branch, and restack the upstack on top.
    
    This is going to be super nice for inserting new branches
    in the middle of an ongoing stack.

--- branch_create.go
+++ branch_create.go
@@ -13,7 +13,9 @@ import (
 type branchCreateCmd struct {
 	Name string `arg:"" optional:"" help:"Name of the new branch"`
 
-	Below   bool   `help:"Place the branch below the current branch."`
+	Insert bool `help:"Restack the upstack of the current branch on top of the new branch"`
+	Below  bool `help:"Place the branch below the current branch. Implies --insert."`
+
 	Message string `short:"m" long:"message" optional:"" help:"Commit message"`
 }
 
@@ -32,7 +34,7 @@ func (cmd *branchCreateCmd) Run(ctx context.Context, log *zerolog.Logger) (err e
 	}
 	trunk := store.Trunk()
 
-	// TODO: guess branch name from commit name
+	// TODO: guess branch name from commit subject
 	if cmd.Name == "" {
 		return errors.New("branch name is required")
 	}
@@ -56,9 +58,9 @@ func (cmd *branchCreateCmd) Run(ctx context.Context, log *zerolog.Logger) (err e
 		Name: currentBranch,
 		Hash: currentHash,
 	}
-	// If trying to insert below current branch,
-	// detach to base instead.
 
+	// Branches to restack on top of new branch.
+	var restackOntoNew []string
 	if cmd.Below {
 		if currentBranch == trunk {
 			log.Error().Msg("--below: cannot create a branch below trunk")
@@ -70,7 +72,20 @@ func (cmd *branchCreateCmd) Run(ctx context.Context, log *zerolog.Logger) (err e
 			return fmt.Errorf("branch not tracked: %v", currentBranch)
 		}
 
+		// If trying to insert below current branch,
+		// detach to base instead,
+		// and restack current branch on top.
 		base = b.Base
+		restackOntoNew = append(restackOntoNew, currentBranch)
+	} else if cmd.Insert {
+		// If inserting, restacking all the upstacks of current branch
+		// onto the new branch.
+		aboves, err := store.ListAbove(ctx, currentBranch)
+		if err != nil {
+			return fmt.Errorf("list branches above %s: %w", currentBranch, err)
+		}
+
+		restackOntoNew = append(restackOntoNew, aboves...)
 	}
 
 	if err := repo.DetachHead(ctx, base.Name); err != nil {
@@ -111,18 +126,22 @@ func (cmd *branchCreateCmd) Run(ctx context.Context, log *zerolog.Logger) (err e
 		return fmt.Errorf("set branch: %w", err)
 	}
 
-	if !cmd.Below {
+	if len(restackOntoNew) == 0 {
 		return nil
 	}
 
-	// Update the base for current branch and restack it.
+	// For --insert and --below, set the base branch of all affected
+	// branches to the newly created branch and run a restack.
+
 	// TODO: should be atomic with the other update
-	if err := store.UpsertBranch(ctx, state.UpsertBranchRequest{
-		Name:    currentBranch,
-		Base:    cmd.Name,
-		Message: fmt.Sprintf("create branch %s below %s", cmd.Name, currentBranch),
-	}); err != nil {
-		return fmt.Errorf("set branch: %w", err)
+	for _, branch := range restackOntoNew {
+		if err := store.UpsertBranch(ctx, state.UpsertBranchRequest{
+			Name:    branch,
+			Base:    cmd.Name,
+			Message: fmt.Sprintf("insert branch %s below %s", cmd.Name, branch),
+		}); err != nil {
+			return fmt.Errorf("set branch: %w", err)
+		}
 	}
 
 	return (&upstackRestackCmd{}).Run(ctx, log)
