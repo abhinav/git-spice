81af31e041783ebc757639e566040941f2440953
Abhinav Gupta <mail@abhinavg.net>
2024-04-09 17:38:19 -0400

    gs branch: Adjust aliases

--- branch.go
+++ branch.go
@@ -2,15 +2,15 @@ package main
 
 type branchCmd struct {
 	Track   branchTrackCmd   `cmd:"" aliases:"tr" help:"Begin tracking a branch with gs"`
-	Untrack branchUntrackCmd `cmd:"" aliases:"utr" help:"Stop tracking a branch with gs"`
+	Untrack branchUntrackCmd `cmd:"" aliases:"untr" help:"Stop tracking a branch with gs"`
 
 	// Creation and destruction
 	Create branchCreateCmd `cmd:"" aliases:"c" help:"Create a new branch"`
-	Delete branchDeleteCmd `cmd:"" aliases:"de" help:"Delete the current branch"`
-	Fold   branchFoldCmd   `cmd:"" aliases:"f" help:"Fold a branch into its base"`
+	Delete branchDeleteCmd `cmd:"" aliases:"rm" help:"Delete the current branch"`
+	Fold   branchFoldCmd   `cmd:"" help:"Fold a branch into its base"`
 
 	// Mutation
 	Edit    branchEditCmd    `cmd:"" aliases:"e" help:"Edit the current branch"`
-	Rename  branchRenameCmd  `cmd:"" aliases:"r" help:"Rename the current branch"`
+	Rename  branchRenameCmd  `cmd:"" aliases:"mv" help:"Rename the current branch"`
 	Restack branchRestackCmd `cmd:"" aliases:"rs" help:"Restack just one branch"`
 }
