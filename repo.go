779d8b68110ca29513b1a97faad3a183bc1f9b53
Abhinav Gupta <mail@abhinavg.net>
2024-05-03 06:29:14 -0700

    WIP: repo sync

--- repo.go
+++ repo.go
@@ -1,5 +1,6 @@
 package main
 
 type repoCmd struct {
-	Init repoInitCmd `cmd:"" aliases:"i" help:"Initialize a repository for stacking"`
+	Init repoInitCmd `cmd:"" aliases:"i" help:"Initialize a repository for gs"`
+	Sync repoSyncCmd `cmd:"" aliases:"s" help:"Pull latest changes from the remote repository"`
 }
---
+++ repo_sync.go
@@ -0,0 +1,10 @@
+package main
+
+type repoSyncCmd struct {
+	// TODO: flag to not delete merged branches
+	// TODO: flag to auto-restack current stack
+}
+
+func (*repoSyncCmd) Run() error {
+	panic("TODO")
+}
