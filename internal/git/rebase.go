daf286a16744c7527422e5bea2790d919c0dffe3
Abhinav Gupta <mail@abhinavg.net>
2024-05-21 20:53:13 -0700

    git/rebase: Detect interrupts and provide a hook (#55)
    If a rebase operation fails because it was interrupted,
    whether due to conflict or because of a user instruction,
    we don't want to fail the entire git-spice operation.
    
    To fix this, we need a means to handle the rebase failure
    with custom logic.
    
    This adds support for a function to be run when a rebase is interrupted.
    The function gets information about the state of the rebase,
    extracted from rebase's internal state.
    
    This isn't used yet by git-spice commands that perform rebases.
    
    Refs #42

--- internal/git/rebase.go
+++ internal/git/rebase.go
@@ -2,9 +2,19 @@ package git
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+
+	"go.abhg.dev/gs/internal/must"
 )
+
+// ErrRebaseInterrupted is returned when a rebase operation is interrupted
+// because of a
+var ErrRebaseInterrupted = errors.New("rebase interrupted")
 
 // RebaseRequest is a request to rebase a branch.
 type RebaseRequest struct {
@@ -33,6 +43,14 @@ type RebaseRequest struct {
 	// with a list of rebase instructions to edit
 	// before starting the rebase operation.
 	Interactive bool
+
+	// InterruptFunc, if set, is called if a rebase operation
+	// is interrupted because of a conflict,
+	// or because the user an instruction to pause the rebase
+	// (e.g. 'edit' or 'break').
+	//
+	// The Rebase function will return the error returned by this function.
+	InterruptFunc func(context.Context, *RebaseState) error
 }
 
 // Rebase runs a git rebase operation with the specified parameters.
@@ -57,14 +75,150 @@ func (r *Repository) Rebase(ctx context.Context, req RebaseRequest) error {
 		args = append(args, req.Branch)
 	}
 
-	err := r.gitCmd(ctx, args...).
-		Stdin(os.Stdin).
-		Stdout(os.Stdout).
-		Stderr(os.Stderr).
-		Run(r.exec)
+	err := r.gitCmd(ctx, args...).Run(r.exec)
 	if err != nil {
-		return fmt.Errorf("rebase: %w", err)
+		originalErr := err
+		if exitErr := new(exec.ExitError); !errors.As(err, &exitErr) {
+			return fmt.Errorf("rebase: %w", err)
+		}
+
+		// If the rebase operation actually ran, but failed,
+		// we might be in the middle of a rebase operation.
+		state, err := r.loadRebaseState(false /* deliberate */)
+		if err != nil {
+			// Rebase probably failed for a different reason,
+			// so no need to log the state read failure verbosely.
+			r.log.Debug("Failed to read rebase state: %v", err)
+			return originalErr
+		}
+
+		if req.InterruptFunc == nil {
+			// The rebase failed, but we don't have a way to handle it.
+			// Return ErrRebaseInterrupted.
+			return errors.Join(ErrRebaseInterrupted, originalErr)
+		}
+
+		return req.InterruptFunc(ctx, state)
+	}
+
+	// If we have rebase state after a successful return,
+	// this was a deliberate break or edit.
+	if state, err := r.loadRebaseState(true /* deliberate */); err == nil {
+		if req.InterruptFunc == nil {
+			return ErrRebaseInterrupted
+		}
+		return req.InterruptFunc(ctx, state)
 	}
 
 	return nil
+}
+
+// RebaseAbort aborts an ongoing rebase operation.
+func (r *Repository) RebaseAbort(ctx context.Context) error {
+	if err := r.gitCmd(ctx, "rebase", "--abort").Run(r.exec); err != nil {
+		return fmt.Errorf("rebase abort: %w", err)
+	}
+	return nil
+}
+
+// RebaseBackend specifies the kind of rebase backend in use.
+//
+// See https://git-scm.com/docs/git-rebase#_behavioral_differences for details.
+type RebaseBackend int
+
+const (
+	// RebaseBackendMerge refers to the "merge" backend.
+	// It is the default backend used by Git,
+	// and handles more corner cases better.
+	RebaseBackendMerge RebaseBackend = iota
+
+	// RebaseBackendApply refers to the "apply" backend.
+	// It is rarely used and may be phased out in the future
+	// if the merge backend gains all of its features.
+	// It is enabled with the --apply flag.
+	RebaseBackendApply
+)
+
+func (b RebaseBackend) String() string {
+	switch b {
+	case RebaseBackendMerge:
+		return "merge"
+	case RebaseBackendApply:
+		return "apply"
+	default:
+		return "unknown"
+	}
+}
+
+// RebaseState holds information about the current state of a rebase operation.
+type RebaseState struct {
+	// Branch is the branch being rebased.
+	Branch string
+
+	// Backend specifies which merge backend is being used.
+	// Merge is the default.
+	// Apply is rarely used and may be phased out in the future.
+	Backend RebaseBackend
+
+	// Deliberate is true if the rebase was interrupted
+	// because of a deliberate user action (e.g. 'edit' or 'break').
+	Deliberate bool
+}
+
+// loadRebaseState loads information about an ongoing rebase.
+//
+// Rebase state is stored inside .git/rebase-merge or .git/rebase-apply
+// depending on the backend in use.
+// See https://github.com/git/git/blob/d8ab1d464d07baa30e5a180eb33b3f9aa5c93adf/wt-status.c#L1711.
+// Inside that directory, we care about the following files:
+//
+//   - head-name: full ref name of the branch being rebased (e.g. refs/heads/main)
+//
+// There's no Git porcelain command to directly get this information.
+func (r *Repository) loadRebaseState(deliberate bool) (*RebaseState, error) {
+	for _, backend := range []RebaseBackend{RebaseBackendApply, RebaseBackendMerge} {
+		stateDir := filepath.Join(r.gitDir, backend.stateDir())
+		if _, err := os.Stat(stateDir); err != nil {
+			if errors.Is(err, os.ErrNotExist) {
+				continue
+			}
+			return nil, fmt.Errorf("check %v: %w", backend, err)
+		}
+
+		head, err := os.ReadFile(filepath.Join(stateDir, "head-name"))
+		if err != nil {
+			if errors.Is(err, os.ErrNotExist) {
+				continue
+			}
+			return nil, fmt.Errorf("read %v head: %w", backend, err)
+		}
+
+		branchRef := strings.TrimSpace(string(head))
+		state := &RebaseState{
+			Branch:     strings.TrimPrefix(branchRef, "refs/heads/"),
+			Backend:    backend,
+			Deliberate: deliberate,
+		}
+
+		return state, nil
+	}
+
+	return nil, errors.New("no rebase in progress")
+}
+
+// stateDir reports the directory inside the .git directory
+// where rebase state is stored.
+//
+// See
+// https://github.com/git/git/blob/d8ab1d464d07baa30e5a180eb33b3f9aa5c93adf/wt-status.c#L1711.
+func (b RebaseBackend) stateDir() string {
+	switch b {
+	case RebaseBackendMerge:
+		return "rebase-merge"
+	case RebaseBackendApply:
+		return "rebase-apply"
+	default:
+		must.Failf("unknown rebase backend: %v", b)
+		return ""
+	}
 }
---
+++ internal/git/rebase_test.go
@@ -0,0 +1,229 @@
+package git_test
+
+import (
+	"context"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/rogpeppe/go-internal/testscript"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.abhg.dev/gs/internal/git"
+	"go.abhg.dev/gs/internal/git/gittest"
+	"go.abhg.dev/gs/internal/logtest"
+	"go.abhg.dev/gs/internal/mockedit"
+	"go.abhg.dev/gs/internal/text"
+)
+
+func TestMain(m *testing.M) {
+	testscript.RunMain(m, map[string]func() int{
+		// mockedit <input>:
+		"mockedit": mockedit.Main,
+	})
+}
+
+func TestRebase_deliberateInterrupt(t *testing.T) {
+	fixture, err := gittest.LoadFixtureScript([]byte(text.Dedent(`
+		as 'Test <test@example.com>'
+		at '2024-05-21T20:30:40Z'
+
+		git init
+		git commit --allow-empty -m 'Initial commit'
+
+		git add foo.txt
+		git commit -m 'Add foo'
+
+		git checkout -b feature
+
+		git add bar.txt
+		git commit -m 'Add bar'
+
+		git add baz.txt
+		git commit -m 'Add baz'
+
+		-- foo.txt --
+		Contents of foo
+
+		-- bar.txt --
+		Contents of bar
+
+		-- baz.txt --
+		Contents of baz
+	`)))
+	require.NoError(t, err)
+	t.Cleanup(fixture.Cleanup)
+
+	ctx := context.Background()
+	repo, err := git.Open(ctx, fixture.Dir(), git.OpenOptions{
+		Log: logtest.New(t),
+	})
+	require.NoError(t, err)
+
+	login(t, "foo")
+
+	// Test cases with no InterruptFunc.
+	// All must see ErrRebaseInterrupted.
+	noFuncTests := []struct {
+		name  string
+		lines []string
+	}{
+		{
+			name: "break",
+			lines: []string{
+				"pick cc51432 Add bar",
+				"break",
+				"pick 7dd9ddf Add baz",
+			},
+		},
+		{
+			name: "edit",
+			lines: []string{
+				"pick cc51432 Add bar",
+				"edit 7dd9ddf Add baz",
+			},
+		},
+	}
+
+	for _, tt := range noFuncTests {
+		t.Run(tt.name, func(t *testing.T) {
+			defer func() {
+				assert.NoError(t, repo.RebaseAbort(ctx))
+			}()
+			mockedit.Expect(t).
+				GiveLines(tt.lines...)
+
+			err = repo.Rebase(ctx, git.RebaseRequest{
+				Branch:      "feature",
+				Upstream:    "main",
+				Interactive: true,
+			})
+			require.Error(t, err)
+			assert.ErrorIs(t, err, git.ErrRebaseInterrupted)
+		})
+	}
+
+	t.Run("InterruptFunc", func(t *testing.T) {
+		defer func() {
+			assert.NoError(t, repo.RebaseAbort(ctx))
+		}()
+
+		// Either test case will do.
+		mockedit.Expect(t).
+			GiveLines(noFuncTests[0].lines...)
+
+		var calledInterrupt bool
+		defer func() {
+			assert.True(t, calledInterrupt, "InterruptFunc was not called")
+		}()
+
+		err = repo.Rebase(ctx, git.RebaseRequest{
+			Branch:      "feature",
+			Upstream:    "main",
+			Interactive: true,
+			InterruptFunc: func(_ context.Context, state *git.RebaseState) error {
+				calledInterrupt = true
+
+				assert.Equal(t, &git.RebaseState{
+					Branch:     "feature",
+					Deliberate: true,
+				}, state)
+				return nil
+			},
+		})
+	})
+}
+
+func TestRebase_unexpectedInterrupt(t *testing.T) {
+	fixture, err := gittest.LoadFixtureScript([]byte(text.Dedent(`
+		as 'Test <test@example.com>'
+		at '2024-05-21T20:30:40Z'
+
+		git init
+		git commit --allow-empty -m 'Initial commit'
+
+		git add foo.txt
+		git commit -m 'Add foo'
+
+		git checkout -b feature
+		git add bar.txt
+		git commit -m 'Add bar'
+
+		git checkout main
+		mv conflicting-bar.txt bar.txt
+		git add bar.txt
+		git commit -m 'Conflicting bar'
+
+		-- foo.txt --
+		Contents of foo
+
+		-- bar.txt --
+		Contents of bar
+
+		-- conflicting-bar.txt --
+		Different contents of foo
+	`)))
+	require.NoError(t, err)
+
+	ctx := context.Background()
+	repo, err := git.Open(ctx, fixture.Dir(), git.OpenOptions{
+		Log: logtest.New(t),
+	})
+	require.NoError(t, err)
+
+	login(t, "user")
+
+	t.Run("noInterruptFunc", func(t *testing.T) {
+		defer func() {
+			assert.NoError(t, repo.RebaseAbort(ctx))
+		}()
+
+		err = repo.Rebase(ctx, git.RebaseRequest{
+			Branch:   "feature",
+			Upstream: "main",
+		})
+		require.Error(t, err)
+		assert.ErrorIs(t, err, git.ErrRebaseInterrupted)
+	})
+
+	t.Run("InterruptFunc", func(t *testing.T) {
+		defer func() {
+			assert.NoError(t, repo.RebaseAbort(ctx))
+		}()
+
+		var calledInterrupt bool
+		defer func() {
+			assert.True(t, calledInterrupt, "InterruptFunc was not called")
+		}()
+
+		err = repo.Rebase(ctx, git.RebaseRequest{
+			Branch:   "feature",
+			Upstream: "main",
+			InterruptFunc: func(_ context.Context, state *git.RebaseState) error {
+				calledInterrupt = true
+
+				assert.Equal(t, &git.RebaseState{
+					Branch: "feature",
+				}, state)
+				return nil
+			},
+		})
+		require.NoError(t, err)
+	})
+}
+
+func login(t testing.TB, username string) (home string) {
+	require.NotEmpty(t, username, "username must not be empty")
+	require.NotContains(t, username, " ", "username must not contain spaces")
+
+	home = filepath.Join(t.TempDir(), username)
+	require.NoError(t, os.MkdirAll(home, 0o700))
+
+	t.Setenv("HOME", home)
+	t.Setenv("GIT_CONFIG_GLOBAL", filepath.Join(home, ".gitconfig"))
+	t.Setenv("GIT_AUTHOR_NAME", username)
+	t.Setenv("GIT_AUTHOR_EMAIL", username+"@example.com")
+	t.Setenv("GIT_COMMITTER_NAME", username)
+	t.Setenv("GIT_COMMITTER_EMAIL", username+"@example.com")
+	return home
+}
