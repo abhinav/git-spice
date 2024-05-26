8d4ae395e0b34f0f9e36a612e1226279ed2d7d43
Abhinav Gupta <mail@abhinavg.net>
2024-05-26 16:47:22 -0700

    branch checkout: Prompt for a branch name if not given (#107)
    To test this, add support to with-term to set the terminal size.

--- branch.go
+++ branch.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"context"
+	"errors"
+	"fmt"
+	"slices"
+	"sort"
+
+	"go.abhg.dev/gs/internal/git"
+	"go.abhg.dev/gs/internal/ui"
+)
+
 type branchCmd struct {
 	Track    branchTrackCmd    `cmd:"" aliases:"tr" help:"Track a branch"`
 	Untrack  branchUntrackCmd  `cmd:"" aliases:"untr" help:"Forget a tracked branch"`
@@ -18,4 +29,57 @@ type branchCmd struct {
 
 	// Pull request management
 	Submit branchSubmitCmd `cmd:"" aliases:"s" help:"Submit a branch"`
+}
+
+// branchPrompt prompts a user to select a branch.
+type branchPrompt struct {
+	// Exclude specifies branches that will not be included in the list.
+	Exclude []string
+
+	// ExcludeCheckedOut specifies whether branches that are checked out
+	// in any worktree should be excluded.
+	ExcludeCheckedOut bool
+
+	// Title specifies the prompt to display to the user.
+	Title string
+
+	// Description specifies the description to display to the user.
+	Description string
+}
+
+func (p *branchPrompt) Run(ctx context.Context, repo *git.Repository) (string, error) {
+	slices.Sort(p.Exclude)
+
+	branches, err := repo.LocalBranches(ctx)
+	if err != nil {
+		return "", fmt.Errorf("list branches: %w", err)
+	}
+
+	branchNames := make([]string, 0, len(branches))
+	for _, branch := range branches {
+		if p.ExcludeCheckedOut && branch.CheckedOut {
+			continue
+		}
+
+		if _, ok := slices.BinarySearch(p.Exclude, branch.Name); ok {
+			continue
+		}
+
+		branchNames = append(branchNames, branch.Name)
+	}
+	sort.Strings(branchNames)
+
+	if len(branchNames) == 0 {
+		return "", errors.New("no branches available")
+	}
+
+	var result string
+	prompt := ui.NewSelect(&result, branchNames...).
+		WithTitle(p.Title).
+		WithDescription(p.Description)
+	if err := ui.Run(prompt); err != nil {
+		return "", fmt.Errorf("select branch: %w", err)
+	}
+
+	return result, nil
 }
