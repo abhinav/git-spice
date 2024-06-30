7176ff8cee7d2fee5ba88937451e22115249d928
Abhinav Gupta <mail@abhinavg.net>
2024-06-30 11:43:55 -0700

    refactor(script_test): Extract shamhub CLI (#236)
    Extract the N different shamhub control command
    into a single 'shamhub' command defined in the shamhub package.
    All state management is done internally.

--- script_test.go
+++ script_test.go
@@ -6,11 +6,7 @@ import (
 	"os"
 	"path/filepath"
 	"reflect"
-	"slices"
-	"strconv"
-	"strings"
 	"testing"
-	"time"
 
 	"github.com/charmbracelet/log"
 	"github.com/rogpeppe/go-internal/diff"
@@ -18,7 +14,6 @@ import (
 	"go.abhg.dev/gs/internal/forge"
 	"go.abhg.dev/gs/internal/forge/shamhub"
 	"go.abhg.dev/gs/internal/git/gittest"
-	"go.abhg.dev/gs/internal/logtest"
 	"go.abhg.dev/gs/internal/mockedit"
 	"go.abhg.dev/gs/internal/termtest"
 )
@@ -49,14 +44,6 @@ func TestMain(m *testing.M) {
 }
 
 func TestScript(t *testing.T) {
-	// We always put a *shamHubValue into the environment
-	// because testscript does not allow adding the value afterwards.
-	// We only set up the ShamHub on gh-init, though.
-	type shamHubKey struct{}
-	type shamHubValue struct{ v *shamhub.ShamHub }
-
-	type testingTBKey struct{}
-
 	defaultEnv := gittest.DefaultConfig().EnvMap()
 	defaultEnv["EDITOR"] = "mockedit"
 
@@ -67,6 +54,8 @@ func TestScript(t *testing.T) {
 	defaultEnv["GIT_COMMITTER_NAME"] = "Test"
 	defaultEnv["GIT_COMMITTER_EMAIL"] = "test@example.com"
 
+	var shamhubCmd shamhub.Cmd
+
 	testscript.Run(t, testscript.Params{
 		Dir:                filepath.Join("testdata", "script"),
 		UpdateScripts:      *_update,
@@ -76,8 +65,7 @@ func TestScript(t *testing.T) {
 				e.Setenv(k, v)
 			}
 
-			e.Values[shamHubKey{}] = &shamHubValue{}
-			e.Values[testingTBKey{}] = e.T().(testing.TB)
+			shamhubCmd.Setup(t, e)
 			return nil
 		},
 		Cmds: map[string]func(*testscript.TestScript, bool, []string){
@@ -92,169 +80,8 @@ func TestScript(t *testing.T) {
 				// can set GIT_SPICE_NOW to a different value.
 				ts.Setenv("GIT_SPICE_NOW", ts.Getenv("GIT_COMMITTER_DATE"))
 			},
-
 			"cmpenvJSON": cmdCmpenvJSON,
-
-			// TODO: rename "gh-*" to "shamhub-*".
-
-			// Sets up a fake code forge called shamhub.
-			// This will be picked up by "gs" commands.
-			"gh-init": func(ts *testscript.TestScript, neg bool, args []string) {
-				t := ts.Value(testingTBKey{}).(testing.TB)
-				shamHub, err := shamhub.New(shamhub.Config{
-					Log: logtest.New(t),
-				})
-				if err != nil {
-					ts.Fatalf("create ShamHub: %s", err)
-				}
-				ts.Defer(func() {
-					if err := shamHub.Close(); err != nil {
-						ts.Logf("close ShamHub: %s", err)
-					}
-				})
-				ts.Value(shamHubKey{}).(*shamHubValue).v = shamHub
-
-				ts.Logf("Set up ShamHub:\n"+
-					"  API URL  = %s\n"+
-					"  Git URL  = %s\n"+
-					"  Git root = %s",
-					shamHub.APIURL(),
-					shamHub.GitURL(),
-					shamHub.GitRoot(),
-				)
-
-				ts.Setenv("SHAMHUB_API_URL", shamHub.APIURL())
-				ts.Setenv("SHAMHUB_URL", shamHub.GitURL())
-			},
-
-			// Clones a repository from the fake GitHub server.
-			"gh-clone": func(ts *testscript.TestScript, neg bool, args []string) {
-				if neg || len(args) != 2 {
-					ts.Fatalf("usage: gh-clone <owner/repo> <dir>")
-				}
-
-				shamHub := ts.Value(shamHubKey{}).(*shamHubValue).v
-				if shamHub == nil {
-					ts.Fatalf("gh-add-remote: ShamHub not initialized")
-				}
-
-				ownerRepo, dir := args[0], args[1]
-				owner, repo, ok := strings.Cut(ownerRepo, "/")
-				if !ok {
-					ts.Fatalf("invalid owner/repo: %s", ownerRepo)
-				}
-
-				err := ts.Exec("git", "clone", shamHub.RepoURL(owner, repo), dir)
-				if err != nil {
-					ts.Fatalf("git clone: %s", err)
-				}
-			},
-
-			// gh-merge <owner/repo> <pr>
-			"gh-merge": func(ts *testscript.TestScript, neg bool, args []string) {
-				if neg || len(args) != 2 {
-					ts.Fatalf("usage: gh-merge <owner/repo> <pr>")
-				}
-
-				shamHub := ts.Value(shamHubKey{}).(*shamHubValue).v
-				if shamHub == nil {
-					ts.Fatalf("gh-merge: ShamHub not initialized")
-				}
-
-				ownerRepo, prStr := args[0], args[1]
-				owner, repo, ok := strings.Cut(ownerRepo, "/")
-				if !ok {
-					ts.Fatalf("invalid owner/repo: %s", ownerRepo)
-				}
-				pr, err := strconv.Atoi(prStr)
-				if err != nil {
-					ts.Fatalf("invalid PR number: %s", err)
-				}
-
-				req := shamhub.MergeChangeRequest{
-					Owner:  owner,
-					Repo:   repo,
-					Number: pr,
-				}
-				if at := ts.Getenv("GIT_COMMITTER_DATE"); at != "" {
-					t, err := time.Parse(time.RFC3339, at)
-					if err != nil {
-						ts.Fatalf("invalid time: %s", err)
-					}
-					req.Time = t
-				}
-
-				if name := ts.Getenv("GIT_COMMITTER_NAME"); name != "" {
-					req.CommitterName = name
-				}
-				if email := ts.Getenv("GIT_COMMITTER_EMAIL"); email != "" {
-					req.CommitterEmail = email
-				}
-
-				ts.Check(shamHub.MergeChange(req))
-			},
-
-			"gh-add-remote": func(ts *testscript.TestScript, neg bool, args []string) {
-				if neg || len(args) != 2 {
-					ts.Fatalf("usage: gh-add-remote <remote> <owner/repo>")
-				}
-
-				shamHub := ts.Value(shamHubKey{}).(*shamHubValue).v
-				if shamHub == nil {
-					ts.Fatalf("gh-add-remote: ShamHub not initialized")
-				}
-
-				remote, ownerRepo := args[0], args[1]
-				owner, repo, ok := strings.Cut(ownerRepo, "/")
-				if !ok {
-					ts.Fatalf("invalid owner/repo: %s", ownerRepo)
-				}
-				repo = strings.TrimSuffix(repo, ".git")
-				repoURL, err := shamHub.NewRepository(owner, repo)
-				if err != nil {
-					ts.Fatalf("create repository: %s", err)
-				}
-
-				ts.Check(ts.Exec("git", "remote", "add", remote, repoURL))
-			},
-
-			"gh-dump-pull": func(ts *testscript.TestScript, neg bool, args []string) {
-				if neg || len(args) > 1 {
-					ts.Fatalf("usage: gh-dump-pull [n]")
-				}
-
-				shamHub := ts.Value(shamHubKey{}).(*shamHubValue).v
-				if shamHub == nil {
-					ts.Fatalf("gh-dump-pulls: ShamHub not initialized")
-				}
-
-				changes, err := shamHub.ListChanges()
-				if err != nil {
-					ts.Fatalf("list changes: %s", err)
-				}
-
-				var give any
-				if len(args) == 0 {
-					give = changes
-				} else {
-					wantChange, err := strconv.Atoi(args[0])
-					if err != nil {
-						ts.Fatalf("invalid PR number: %s", err)
-					}
-
-					idx := slices.IndexFunc(changes, func(c *shamhub.Change) bool {
-						return c.Number == wantChange
-					})
-					if idx < 0 {
-						ts.Fatalf("PR %d not found", wantChange)
-					}
-					give = changes[idx]
-				}
-
-				enc := json.NewEncoder(ts.Stdout())
-				enc.SetIndent("", "  ")
-				ts.Check(enc.Encode(give))
-			},
+			"shamhub":    shamhubCmd.Run,
 		},
 	})
 }
