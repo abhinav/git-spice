1d887bc4c9d0d0efe56046490e64f7484ff8abca
Abhinav Gupta <mail@abhinavg.net>
2024-07-03 19:17:34 -0700

    git storage: Don't commit if there are no changes
    If the tree didn't change, we don't need to commit.
    
    [skip changelog]: Not a user facing change.

---
+++ internal/storage/git_test.go
@@ -0,0 +1,41 @@
+package storage
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.abhg.dev/gs/internal/git"
+	"go.abhg.dev/gs/internal/logtest"
+)
+
+func TestGitBackendUpdateNoChanges(t *testing.T) {
+	ctx := context.Background()
+	repo, err := git.Init(ctx, t.TempDir(), git.InitOptions{
+		Log: logtest.New(t),
+	})
+	require.NoError(t, err)
+
+	backend := NewGitBackend(GitConfig{
+		Repo:        repo,
+		Ref:         "refs/data",
+		AuthorName:  "Test Author",
+		AuthorEmail: "test@example.com",
+		Log:         logtest.New(t),
+	})
+
+	db := NewDB(backend)
+	require.NoError(t, db.Set(ctx, "foo", "bar", "initial set"))
+
+	start, err := repo.PeelToCommit(ctx, "refs/data")
+	require.NoError(t, err)
+
+	require.NoError(t, db.Set(ctx, "foo", "bar", "shrug"))
+
+	end, err := repo.PeelToCommit(ctx, "refs/data")
+	require.NoError(t, err)
+
+	assert.Equal(t, start, end,
+		"there should be no changes in the repository")
+}
