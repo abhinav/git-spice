b566d05eec6abcbeadab98293872e9e3504abb11
Abhinav Gupta <mail@abhinavg.net>
2024-11-29 12:27:54 -0800

    commit amend: deprecate -n in favor of --no-edit (#502)
    `git commit --no-edit` does not have a `-n` short form
    so we shouldn't either.
    
    In the interest of not breaking anyone, just deprecate the flag first.
    We'll warn users when they use the short form for now.
    
    Refs
    https://github.com/abhinav/git-spice/issues/380#issuecomment-2508558831

--- commit_amend.go
+++ commit_amend.go
@@ -14,7 +14,12 @@ import (
 type commitAmendCmd struct {
 	All     bool   `short:"a" help:"Stage all changes before committing."`
 	Message string `short:"m" placeholder:"MSG" help:"Use the given message as the commit message."`
-	NoEdit  bool   `short:"n" help:"Don't edit the commit message"`
+
+	NoEdit bool `help:"Don't edit the commit message"`
+
+	// TODO:
+	// Remove this short form and put it on NoVerify.
+	NoEditDeprecated bool `hidden:"true" short:"n" help:"Don't edit the commit message"`
 }
 
 func (*commitAmendCmd) Help() string {
@@ -27,6 +32,11 @@ func (*commitAmendCmd) Help() string {
 }
 
 func (cmd *commitAmendCmd) Run(ctx context.Context, log *log.Logger, view ui.View) error {
+	if cmd.NoEditDeprecated {
+		cmd.NoEdit = true
+		log.Warn("flag '-n' is deprecated; use '--no-edit' instead")
+	}
+
 	repo, err := git.Open(ctx, ".", git.OpenOptions{
 		Log: log,
 	})
