fd1a46880fb53c5d787f78d8a16b29f1d44ed7f0
Abhinav Gupta <mail@abhinavg.net>
2025-05-18 10:31:38 -0700

    wip: commit fixup command

--- commit.go
+++ commit.go
@@ -4,4 +4,5 @@ type commitCmd struct {
 	Create commitCreateCmd `cmd:"" aliases:"c" help:"Create a new commit"`
 	Amend  commitAmendCmd  `cmd:"" aliases:"a" help:"Amend the current commit"`
 	Split  commitSplitCmd  `cmd:"" aliases:"sp" help:"Split the current commit"`
+	Fixup  commitFixupCmd  `cmd:"" aliases:"f" help:"Fixup a commit below the current commit"`
 }
