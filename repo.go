102859655a8d0c136bf7a3e8cd4b3b893be736e3
Abhinav Gupta <mail@abhinavg.net>
2025-07-09 19:52:42 -0700

    feat: repo restack command (#729)
    Add a 'repo restack' command that restacks all tracked branches.
    Branches have to be sorted in topological order
    based on their base relationships before being restacked.
    
    Resolves #505

--- repo.go
+++ repo.go
@@ -1,6 +1,7 @@
 package main
 
 type repoCmd struct {
-	Init repoInitCmd `cmd:"" aliases:"i" help:"Initialize a repository"`
-	Sync repoSyncCmd `cmd:"" aliases:"s" help:"Pull latest changes from the remote"`
+	Init    repoInitCmd    `cmd:"" aliases:"i" help:"Initialize a repository"`
+	Sync    repoSyncCmd    `cmd:"" aliases:"s" help:"Pull latest changes from the remote"`
+	Restack repoRestackCmd `cmd:"" aliases:"r" help:"Restack all tracked branches" released:"unreleased"`
 }
